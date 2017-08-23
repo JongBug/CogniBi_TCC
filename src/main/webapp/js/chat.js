cc9c4bb36f3a7fddb986fed13d1e9a0e890893d6
caique_cassemiro <caiquecassemiro@gmail.com>
2017-08-22 23:38:34 -0300

    Plotando mensagens de retorno do CogniBi

--- src/main/webapp/js/chat.js
+++ src/main/webapp/js/chat.js
@@ -1,62 +1,96 @@
 var appCogniBi = angular.module('appCogniBi', []);
-  appCogniBi.controller('appCogniBiCtrl',['$scope','$http', function($scope, $http){
+appCogniBi.controller('appCogniBiCtrl', ['$scope', '$http', function ($scope, $http) {
 
-      var btnEnviaMensagem = document.querySelector("#btn-envia-mensagem");
-      btnEnviaMensagem.addEventListener("click", function () {
-        event.preventDefault(); // Evita recarregamento da página
+  var btnEnviaMensagem = document.querySelector("#btn-envia-mensagem");
+  btnEnviaMensagem.addEventListener("click", function () {
+    event.preventDefault(); // Evita recarregamento da página
 
-        var formularioChat = document.querySelector("#mensagem-conversa");
-        var textoPergunta = formularioChat.mensagem.value;
+    var formularioChat = document.querySelector("#mensagem-conversa");
+    var textoPergunta = formularioChat.mensagem.value;
 
-        if (textoPergunta != "") {
-            var balaoComMensagem = montaBalaoComMensagem(textoPergunta);
+    if (textoPergunta != "") {
+      var balaoComMensagemUser = montaBalaoComMensagemUser(textoPergunta);
+      var conversa = document.querySelector("#conversa");
+      conversa.appendChild(balaoComMensagemUser);
+      conversa.scrollTop = conversa.scrollHeight;
 
-            // Comunicao com servidor
-            var baseUrl = 'rest/cognibi/chat/' + textoPergunta;
-            $http.get(baseUrl).then(function(response) {
-            	//AQUI EH A RESPOSTA DO SERVIDOR RESPONSE.DATA.CONVERSA
-            	$scope.oi = response.data.conversa;
-            }, function(err) {
-            console.log(err);
-            });
-            //
 
-            var conversa = document.querySelector("#conversa");
-            conversa.appendChild(balaoComMensagem);
-            conversa.scrollTop = conversa.scrollHeight;
+      
+      // Comunicao com servidor
+      var baseUrl = 'rest/cognibi/chat/' + textoPergunta;
+      $http.get(baseUrl).then(function (response) {
+        //AQUI EH A RESPOSTA DO SERVIDOR RESPONSE.DATA.CONVERSA
+        $scope.oi = response.data.conversa;
+        var balaoComMensagemCog = montaBalaoComMensagemCog(response.data.conversa);
+        conversa.appendChild(balaoComMensagemCog);
+        conversa.scrollTop = conversa.scrollHeight;
 
-            formularioChat.reset();
-        } else {
-            return alert("Digite uma mensagem para que eu possa te ajudar");
-        }
+      }, function (err) {
+        console.log(err);
       });
 
+      formularioChat.reset();
+    } else {
+      var conversa = document.querySelector("#conversa");
+      var balaoComMensagemCog = montaBalaoComMensagemCog('Digite uma mensagem para que eu possa te ajudar');
+        conversa.appendChild(balaoComMensagemCog);
+        conversa.scrollTop = conversa.scrollHeight;
+    }
+  });
 
-      function montaBalaoComMensagem(mensagem) {
-        var balaoComMensagem = document.createElement("div");
-        balaoComMensagem.classList.add("row");
-        balaoComMensagem.appendChild(montaBalao(mensagem));
 
-        return balaoComMensagem;
-      };
+  function montaBalaoComMensagemUser(mensagem) {
+    var balaoComMensagem = document.createElement("div");
+    balaoComMensagem.classList.add("row");
+    balaoComMensagem.appendChild(montaBalaoUser(mensagem));
 
-      function montaBalao(mensagem) {
-        var balao = document.createElement("div");
-        balao.classList.add("mensagem-usuario");
-        balao.classList.add("right");
-        balao.appendChild(montaMensagem(mensagem));
+    return balaoComMensagem;
+  };
 
-        return balao;
-      };
+  function montaBalaoUser(mensagem) {
+    var balao = document.createElement("div");
+    balao.classList.add("mensagem-usuario");
+    balao.classList.add("right");
+    balao.appendChild(montaMensagemUser(mensagem));
 
-      function montaMensagem(mensagem) {
-        var montaMensagem = document.createElement("p");
-        montaMensagem.classList.add("texto-mensagem");
-        montaMensagem.classList.add("grey-text");
-        montaMensagem.classList.add("text-darken-4")
-        montaMensagem.textContent = mensagem;
+    return balao;
+  };
 
-        return montaMensagem;
-      };
+  function montaMensagemUser(mensagem) {
+    var montaMensagem = document.createElement("p");
+    montaMensagem.classList.add("texto-mensagem");
+    montaMensagem.classList.add("grey-text");
+    montaMensagem.classList.add("text-darken-4")
+    montaMensagem.textContent = mensagem;
+
+    return montaMensagem;
+  };
+
+  function montaBalaoComMensagemCog(mensagem) {
+    var balaoComMensagem = document.createElement("div");
+    balaoComMensagem.classList.add("row");
+    balaoComMensagem.appendChild(montaBalaoCog(mensagem));
+
+    return balaoComMensagem;
+  };
+
+  function montaBalaoCog(mensagem) {
+    var balao = document.createElement("div");
+    balao.classList.add("balao-mensagem-cognibi");
+    balao.classList.add("left");
+    balao.appendChild(montaMensagemCog(mensagem));
+
+    return balao;
+  };
+
+  function montaMensagemCog(mensagem) {
+    var montaMensagem = document.createElement("p");
+    montaMensagem.classList.add("texto-mensagem");
+    montaMensagem.classList.add("white-text");
+    //montaMensagem.classList.add("text-darken-4")
+    montaMensagem.textContent = mensagem;
+
+    return montaMensagem;
+  };
 
 }]);
